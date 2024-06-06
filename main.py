0eebcbb79a88b3277082bd8f3b54fdaf18cbc114
Harshit Yadav <2105891@kiit.ac.in>
2024-06-06 22:03:00 +0530

    feat: binary tree from heap

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ class BinaryTree:
         self.left = None
         self.right = None
 
+    def __less_than__(self, other):
+            return self.freq < other.freq
+    
+    def __equals__(self, other):
+            return self.freq == other.freq
+
 class Huffmancode:
 
     def __init__(self, path):
         self.path = path
         self.__heap =[]
-
-    def __less_than__(self, other):
-            return self.frequency < other.frequency
-    
-    def __equals__(self, other):
-            return self.frequeny == other.frequency
 
     def __frequency_from_text(self,text):
         frequency_dict = {}
@@ -32,8 +32,19 @@ class Huffmancode:
         for key in frequency_dict:
             frequency = frequency_dict[key]
             binary_tree_node = BinaryTree(key, frequency)
-            heapq.push(self.__heap, binary_tree_node)
+            heapq.heappush(self.__heap, binary_tree_node)
 
+
+    def __Build_Binary_Tree(self):
+        while len(self.__heap) > 1:
+            binary_tree_node1 = heapq.heappop(self.__heap)
+            binary_tree_node2 = heapq.heappop(self.__heap)
+            freq_sum = binary_tree_node1.freq + binary_tree_node2.freq
+            newNode = BinaryTree(None, freq_sum)
+            newNode.left = binary_tree_node1
+            newNode.right = binary_tree_node2
+            # heapq.heappush(self.__heap, newNode)
+        return
 
     def compression(self): 
         # To access the file and extract text from that file
@@ -43,6 +54,7 @@ class Huffmancode:
         # create min heap for two minimum frequency
         build_heap = self.__Build_heap(frequency_dictionary)
         # construct binary tree from heap
+        self.__Build_Binary_Tree()
         # construct code from binary tree and store it in dictionary
         # construct encoder text.
         # we have return that binary file as an output
